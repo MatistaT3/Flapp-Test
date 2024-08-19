4c797c1ded83a89a6a74b067099a0ad1f8172a4f
Matias Meza <matista2.0@gmail.com>
2024-08-19 16:59:47 -0400

    Finished the backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,29 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import cartRoutes from './routes/cart.routes';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 4000;
 
 app.use(express.json());
+app.use(
+  '/',
+  express.static(path.join(__dirname, 'public'), { extensions: ['html'] })
+);
+app.use('/_next', express.static(path.join(__dirname, 'public/_next')));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:4000',
+      'http://localhost:4001',
+    ],
+  })
+);
+
+app.use(cartRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
